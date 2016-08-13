b5594e2a0df6c88ea8190ae5b38bd44db993e534
Ben Strutt <b.strutt.12@ucl.ac.uk>
2016-08-13 17:49:24 +0100

    can loop over events sensibly

--- fullDataSetPerformance.cxx
+++ fullDataSetPerformance.cxx
@@ -41,35 +41,33 @@ int main(int argc, char *argv[])
   if(argc==3){std::cout << "\t" << argv[2];}
   std::cout << std::endl;
   const Int_t firstRun = atoi(argv[1]);
-  const Int_t lastRun = firstRun; //argc==3 ? atoi(argv[2]) : firstRun;
-  const Double_t maxDeltaTriggerTimeNs = 1200;
+  const Int_t lastRun = atoi(argv[2]);; //argc==3 ? atoi(argv[2]) : firstRun;
 
   
   TChain* headChain = new TChain("headTree");
   // TChain* gpsChain = new TChain("adu5PatTree");
-  TChain* angResChain = new TChain("angResChain");  
+  TChain* angResChain = new TChain("angResTree");  
 
   for(Int_t run=firstRun; run<=lastRun; run++){
+
     TString fileName = TString::Format("~/UCL/ANITA/flight1415/root/run%d/headFile%d.root", run, run);
     headChain->Add(fileName);
-    // fileName = TString::Format("~/UCL/ANITA/flight1415/root/run%d/gpsEvent%d.root", run, run);
-    // gpsChain->Add(fileName);
-    fileName = TString::Format("~/UCL/ANITA/anita3Analysis/final/filterOffs/phaseCentre/generateAngularResolutionTreePlots%d.root");
-    angResChain->Add(fileName);    
+
+    
+    fileName = TString::Format("~/UCL/ANITA/anita3Analysis/antennaPositionCalib/final/filterOffs/phaseCenter/generateAngularResolutionTreePlots_%d_*.root", run);
+    angResChain->Add(fileName);
   }
   RawAnitaHeader* header = NULL;
   headChain->SetBranchAddress("header", &header);
-  // Adu5Pat* pat = NULL;
-  // gpsChain->SetBranchAddress("pat", &pat);
 
 
   UInt_t eventNumber = 0;
   angResChain->SetBranchAddress("eventNumber", &eventNumber);
-  UInt_t zoomPhiDeg = 0;
+  Double_t zoomPhiDeg = 0;
   angResChain->SetBranchAddress("zoomPhiDeg", &zoomPhiDeg);
-  UInt_t zoomThetaDeg = 0;
+  Double_t zoomThetaDeg = 0;
   angResChain->SetBranchAddress("zoomThetaDeg", &zoomThetaDeg);
-  UInt_t zoomPeak = 0;
+  Double_t zoomPeak = 0;
   angResChain->SetBranchAddress("zoomPeak", &zoomPeak);
   
   OutputConvention oc(argc, argv);
@@ -89,18 +87,31 @@ int main(int argc, char *argv[])
   std::cout << "Processing " << maxEntry << " of " << nEntries << " entries." << std::endl;
   ProgressBar p(maxEntry-startEntry);
 
-  for(Long64_t entry = startEntry; entry < maxEntry; entry++){
 
+  TH1D* hPhi = new TH1D("hPhi", "#phi", 1024, -180, 180);
+  TH1D* hTheta = new TH1D("hTheta", "#theta", 1024, -90, 90);
+  TH1D* hPeak = new TH1D("hPeak", "peak", 1024, -1, 1);
+  
+  for(Long64_t entry = startEntry; entry < maxEntry; entry++){
     
     angResChain->GetEntry(entry);
+
     headChain->GetEntryWithIndex(eventNumber);
 
     Float_t priPeak = header->getImagePeak();
     Float_t priPhiDeg = header->getPeakPhiDeg();
     Float_t priThetaDeg = header->getPeakThetaDeg();
+    Int_t priority = header->priority;
+    Float_t hilbertPeak = header->getPeakPhiRad();
     
+    // std::cerr << entry << "\t" << eventNumber << "\t" << priPeak << "\t" << priPhiDeg << "\t" << priThetaDeg << "\t" << std::endl;
 
+    // std::cerr << entry << "\t" << eventNumber << "\t" << zoomPeak << "\t" << zoomPhiDeg << "\t" << zoomThetaDeg << "\t" << std::endl;    
     
+    hPhi->Fill(RootTools::getDeltaAngleDeg((Double_t)priPhiDeg, zoomPhiDeg));
+    hTheta->Fill((Double_t)priThetaDeg - zoomThetaDeg);
+    hPeak->Fill((Double_t)priPeak - zoomPeak);    
+        
     p.inc(entry, maxEntry);
   }
   
